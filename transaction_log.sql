b8c54cd87e17737292e5177396963bdb587d4fd8
rickyp72 <2026055+rickyp72@users.noreply.github.com>
2021-02-19 17:33:07 +0000

    password secret manager test

--- transaction_log.sql
+++ transaction_log.sql
@@ -30,9 +30,9 @@ $do$
 BEGIN
    IF NOT EXISTS (
       SELECT FROM pg_catalog.pg_roles
-      WHERE  rolname = 'runtime_user3') THEN
+      WHERE  rolname = 'runtime_user4') THEN
 
-        CREATE ROLE runtime_user3 WITH
+        CREATE ROLE runtime_user4 WITH
         LOGIN
         NOSUPERUSER
         NOCREATEDB
