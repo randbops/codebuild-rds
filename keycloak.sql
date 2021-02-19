56147d1f65e8eb99216bf445f29ed3dd371add1e
rickyp72 <2026055+rickyp72@users.noreply.github.com>
2021-02-19 14:34:30 +0000

    add sql file

--- keycloak.sql
+++ keycloak.sql
@@ -1,11 +1,21 @@
-CREATE ROLE keycloak_user WITH
-LOGIN
-NOSUPERUSER
-NOCREATEDB
-NOCREATEROLE
-INHERIT
-NOREPLICATION
-CONNECTION LIMIT -1
-PASSWORD 'seine123';
+DO
+$do$
+BEGIN
+   IF NOT EXISTS (
+      SELECT FROM pg_catalog.pg_roles  -- SELECT list can be empty for this
+      WHERE  rolname = 'rp_test_user') THEN
 
-COMMENT ON ROLE keycloak_user IS 'User used by the UK ETS application to perform DB operations.';
+      CREATE ROLE keycloak_user WITH
+      LOGIN
+      NOSUPERUSER
+      NOCREATEDB
+      NOCREATEROLE
+      INHERIT
+      NOREPLICATION
+      CONNECTION LIMIT -1
+      PASSWORD 'seine123';
+
+      COMMENT ON ROLE keycloak_user IS 'User used by the UK ETS application to perform DB operations.';
+   END IF;
+END
+$do$;
