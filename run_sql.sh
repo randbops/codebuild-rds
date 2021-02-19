84ec641326bcd864710fca3c0461e8f2930f91da
rickyp72 <2026055+rickyp72@users.noreply.github.com>
2021-02-19 16:57:48 +0000

    password secret manager test

--- run_sql.sh
+++ run_sql.sh
@@ -1,7 +1,8 @@
 TRAN_RUNTIME_PW=$1
 
 
-sql=$(sed "s/\TRAN_RUNTIME_PW/$TRAN_TIME_PW/" transaction_log.sql)
+sql=$(sed "s/\TRAN_RUNTIME_PW/$TRAN_RUNTIME_PW/" transaction_log.sql)
 psql -U postgres -h database-1.c1lhknn6bc0m.eu-west-2.rds.amazonaws.com -w -d postgres <<EOF
 $sql
 EOF
+echo $sql
--- transaction_log.sql
+++ transaction_log.sql
@@ -30,9 +30,9 @@ $do$
 BEGIN
    IF NOT EXISTS (
       SELECT FROM pg_catalog.pg_roles
-      WHERE  rolname = 'runtime_user_test4') THEN
+      WHERE  rolname = 'runtime_user_test7') THEN
 
-        CREATE ROLE runtime_user_test4 WITH
+        CREATE ROLE runtime_user_test7 WITH
         LOGIN
         NOSUPERUSER
         NOCREATEDB
