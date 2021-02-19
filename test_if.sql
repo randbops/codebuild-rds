66f13bd64b4e4a424b909ea9bcdb3d961f4573aa
rickyp72 <2026055+rickyp72@users.noreply.github.com>
2021-02-19 14:17:13 +0000

    add sql file

--- test_if.sql
+++ test_if.sql
@@ -2,7 +2,7 @@ DO
 $do$
 BEGIN
    IF NOT EXISTS (
-      SELECT FROM pg_cata.pg_roles  -- SELECT list can be empty for this
+      SELECT FROM pg_catalog.pg_roles  -- SELECT list can be empty for this
       WHERE  rolname = 'rp_test_user') THEN
 
       CREATE ROLE rp_test_user LOGIN PASSWORD 'my_password';
