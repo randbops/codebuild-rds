86beb0de4fd34da9986e4c4ecc814fc663b88924
rickyp72 <2026055+rickyp72@users.noreply.github.com>
2021-02-20 08:18:57 +0000

    user7 test without SM in codebuild gui

---
+++ transaction_runtime.sql
@@ -0,0 +1,25 @@
+DO
+$do$
+BEGIN
+   IF NOT EXISTS (
+      SELECT FROM pg_catalog.pg_roles
+      WHERE  rolname = 'runtime_user8') THEN
+
+        CREATE ROLE runtime_user8 WITH
+        LOGIN
+        NOSUPERUSER
+        NOCREATEDB
+        NOCREATEROLE
+        INHERIT
+        NOREPLICATION
+        CONNECTION LIMIT -1
+        PASSWORD 'PASSWORD';
+
+        COMMENT ON ROLE runtime_user IS 'User used by the UK ETS application to perform DB operations.';
+
+        GRANT SELECT, INSERT, UPDATE, DELETE, TRUNCATE, REFERENCES, TRIGGER
+        ON ALL TABLES IN SCHEMA public TO runtime_user;
+
+   END IF;
+END
+$do$;
--- working_run_sql.sh
+++ working_run_sql.sh
@@ -1,9 +1,9 @@
-PASSWORD=$1
+TRAN_RUNTIME_PW=$1
 HOST='database-1.c1lhknn6bc0m.eu-west-2.rds.amazonaws.com'
 DATABASE='postgres'
-SQL_FILE='transaction_runtime.sql'
+SQL_FILE='transaction_log.sql'
 
-sql=$(sed "s/\PASSWORD/$PASSWORD/" $SQL_FILE)
+sql=$(sed "s/\TRAN_RUNTIME_PW/$TRAN_RUNTIME_PW/" $SQL_FILE)
 psql -U postgres -h $HOST -w -d $DATABASE <<EOF
 $sql
 EOF
