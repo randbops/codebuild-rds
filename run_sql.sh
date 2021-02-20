06a89e2b147b2ff3182c11f7c1c581f4837324d9
rickyp72 <2026055+rickyp72@users.noreply.github.com>
2021-02-20 07:25:25 +0000

    sh vars

--- run_sql.sh
+++ run_sql.sh
@@ -1,8 +1,10 @@
 TRAN_RUNTIME_PW=$1
+HOST='database-1.c1lhknn6bc0m.eu-west-2.rds.amazonaws.com'
+DATABASE='postgres'
+SQL_FILE='transaction_log.sql'
 
-
-sql=$(sed "s/\TRAN_RUNTIME_PW/$TRAN_RUNTIME_PW/" transaction_log.sql)
-psql -U postgres -h database-1.c1lhknn6bc0m.eu-west-2.rds.amazonaws.com -w -d postgres <<EOF
+sql=$(sed "s/\TRAN_RUNTIME_PW/$TRAN_RUNTIME_PW/" $SQL_FILE)
+psql -U postgres -h $HOST -w -d $DATABASE <<EOF
 $sql
 EOF
 echo $sql
