6c435462cd186efb3089b30f1d605a0defca065f
rickyp72 <2026055+rickyp72@users.noreply.github.com>
2021-02-20 08:05:54 +0000

    user7 test without SM in codebuild gui

--- transaction_log.sql
+++ transaction_log.sql
@@ -30,9 +30,9 @@ $do$
 BEGIN
    IF NOT EXISTS (
       SELECT FROM pg_catalog.pg_roles
-      WHERE  rolname = 'runtime_user6') THEN
+      WHERE  rolname = 'runtime_user7') THEN
 
-        CREATE ROLE runtime_user6 WITH
+        CREATE ROLE runtime_user7 WITH
         LOGIN
         NOSUPERUSER
         NOCREATEDB
