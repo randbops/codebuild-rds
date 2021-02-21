578f14abfc4e86434916a242a584e1328f3dd87b
rickyp72 <2026055+rickyp72@users.noreply.github.com>
2021-02-21 22:24:29 +0000

    user18

--- transaction_runtime.sql
+++ transaction_runtime.sql
@@ -3,9 +3,9 @@ $do$
 BEGIN
    IF NOT EXISTS (
       SELECT FROM pg_catalog.pg_roles
-      WHERE  rolname = 'runtime_user17') THEN
+      WHERE  rolname = 'runtime_user18') THEN
 
-        CREATE ROLE runtime_user17 WITH
+        CREATE ROLE runtime_user18 WITH
         LOGIN
         NOSUPERUSER
         NOCREATEDB
