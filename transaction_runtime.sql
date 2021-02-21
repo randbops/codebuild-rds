9d989f2e92494094bcd5dd706d5d1d9380b36f10
rickyp72 <2026055+rickyp72@users.noreply.github.com>
2021-02-21 22:13:58 +0000

    remove variables in the buildspec

--- transaction_runtime.sql
+++ transaction_runtime.sql
@@ -3,9 +3,9 @@ $do$
 BEGIN
    IF NOT EXISTS (
       SELECT FROM pg_catalog.pg_roles
-      WHERE  rolname = 'runtime_user16') THEN
+      WHERE  rolname = 'runtime_user17') THEN
 
-        CREATE ROLE runtime_user16 WITH
+        CREATE ROLE runtime_user17 WITH
         LOGIN
         NOSUPERUSER
         NOCREATEDB
